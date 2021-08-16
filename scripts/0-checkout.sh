078ee44dffdfe6e420ceb73128bc3ede6a82cf56
akathirvel <39790038+akathirvel@users.noreply.github.com>
2021-08-16 16:42:52 +0200

    Update 0-checkout.sh

--- scripts/0-checkout.sh
+++ scripts/0-checkout.sh
@@ -3,12 +3,7 @@
 _SRC_REPO_URL=$1
 _BRANCH=$2
 ssh_key=$3
-echo "Creating floder....."
-mkdir /root/.ssh
-echo "DONE Creating floder....."
-echo "$ssh_key" >> /root/.ssh/id_rsa
-chmod 400 /root/.ssh/id_rsa
-ssh-keyscan github.com > /root/.ssh/known_hosts
+
  
 git clone -b $_BRANCH  https://akathirvel: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@github.com/akathirvel/basic-nextjs.git src
 (cd src; git log -1)
