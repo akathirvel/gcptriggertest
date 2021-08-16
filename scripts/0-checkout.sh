7521998b9096a6968790f153e6ecefe7c7bd7a17
akathirvel <39790038+akathirvel@users.noreply.github.com>
2021-08-16 13:47:41 +0200

    Create 0-checkout.sh

---
+++ scripts/0-checkout.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+_SRC_REPO_URL=$1
+_BRANCH=$2
+ssh_key=$3
+
+mkdir /root/.ssh
+echo "$ssh_key" >> /root/.ssh/id_rsa
+chmod 400 /root/.ssh/id_rsa
+ssh-keyscan github.com > /root/.ssh/known_hosts
+
+git clone -b $_BRANCH $_SRC_REPO_URL src
+(cd src; git log -1)
