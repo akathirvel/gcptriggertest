f928462bc1fa41386f3d15737629a6e7b2a24ce4
akathirvel <39790038+akathirvel@users.noreply.github.com>
2021-08-16 16:43:23 +0200

    Update 0-checkout.sh

--- scripts/0-checkout.sh
+++ scripts/0-checkout.sh
@@ -4,6 +4,6 @@ _SRC_REPO_URL=$1
 _BRANCH=$2
 ssh_key=$3
 
- 
+ echo "Helloooooo Angles "
 git clone -b $_BRANCH  https://akathirvel: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@github.com/akathirvel/basic-nextjs.git src
 (cd src; git log -1)
