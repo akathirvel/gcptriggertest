412457889a12d59578fc047522019d6c0cb960ca
akathirvel <39790038+akathirvel@users.noreply.github.com>
2021-08-16 16:13:34 +0200

    Update 0-checkout.sh

--- scripts/0-checkout.sh
+++ scripts/0-checkout.sh
@@ -3,8 +3,9 @@
 _SRC_REPO_URL=$1
 _BRANCH=$2
 ssh_key=$3
-
+echo "Creating floder....."
 mkdir /root/.ssh
+echo "DONE Creating floder....."
 echo "$ssh_key" >> /root/.ssh/id_rsa
 chmod 400 /root/.ssh/id_rsa
 ssh-keyscan github.com > /root/.ssh/known_hosts
