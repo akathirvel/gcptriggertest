8197f931714e6a720dcd72021052dec15eb0ea4f
akathirvel <39790038+akathirvel@users.noreply.github.com>
2021-08-17 13:51:28 +0200

    Update display_date.sh

--- scripts/display_date.sh
+++ scripts/display_date.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 _SRC_REPO_URL=$1
 _BRANCH=$2
-_OTHR=$3
+_REPO=$3
 
 echo -e 'Source code location is $_SRC_REPO_URL'
 echo -e 'Branch  is $_BRANCH'
-echo -e '_OTHR is $_OTHR'
+echo -e '_OTHR is $_REPO'
 echo $_SRC_REPO_URL
 echo $_BRANCH
-echo $_OTHR
+echo $_REPO
 
 
 if (($(date '+%-e')%2)); then
