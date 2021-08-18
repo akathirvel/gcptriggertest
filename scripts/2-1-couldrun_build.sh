e26ccade3b20634e14bc447d76571a6c563a230b
akathirvel <39790038+akathirvel@users.noreply.github.com>
2021-08-18 12:45:45 +0200

    Update 2-1-couldrun_build.sh

--- scripts/2-1-couldrun_build.sh
+++ scripts/2-1-couldrun_build.sh
@@ -4,7 +4,7 @@ BRANCH=$2
 
 echo -e "building branch $BRANCH with env vars:\n$(cat /workspace/src/devops/_docker-build-env)"
 echo "---"
-REPO2 = ${REPO,,}
-BRANCH2 = ${BRANCH,,}
+REPO2=${REPO,,}
+BRANCH2=${BRANCH,,}
 docker build -t gcr.io/NextJsProject/$REPO2-$BRANCH2 -f $DOCKERFILE /workspace/src
 docker push gcr.io/NextJsProject/$REPO2-$BRANCH2
